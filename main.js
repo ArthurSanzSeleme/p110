9ff6b93fbb84541ffc4fa69242d9b2fc4816da09
ArthurSanzSeleme <133713885+ArthurSanzSeleme@users.noreply.github.com>
2023-08-18 15:10:22 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,36 @@
-//https://teachablemachine.withgoogle.com/models/bgKVNWlaH/
+
+predicition1 ="";
+predicition2 ="";
+
+
+Webcam.set({
+width:350,
+height:300,
+imageFormat : 'png',
+pngQuality:90
+});
+
+camera = document.getElementById("camera");
+
+Webcam.attach('#camera');
+
+function takeSnapshot(){
+   Webcam.snap(function(data_uri){
+    document.getElementById("result").innerHTML = '<img id="capture_image" src="'+data_uri+'"/>';
+   }) 
+}
+console.log('ml5 version:', ml5.version);
+
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/bgKVNWlaH/model.json',modelLoaded);
+
+function modelLoaded(){
+  console.log('Model Loaded!');  
+}
+
+function speak(){
+    var synth = window.speechSynthesis;
+    speakData1 = "A primeira previsão é" + predicition1;
+    speakData2 = "E a segunda previsão é" + predicition2;
+    var utterThis = new SpeechSynthesisUtterance(speakData1 + speakData2);
+    synth.speak(utterThis);
+}
